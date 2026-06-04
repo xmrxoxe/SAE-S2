4092f9e4c508b2285724fc0caff02f0997200a83
Mary <marionbochud@gmail.com>
2026-06-04 08:24:37 +0200

    commit 1

---
+++ script.js
@@ -0,0 +1,15 @@
+let select = document.getElementById('select');
+let list_options = document.getElementById('list_options');
+let selectOptions = document.getElementById('select_options');
+let options = document.getElementsByClassName('options');
+let searchInput = document.getElementById('search_input');
+
+select.onclick = function() {
+    list_options.classList.toggle('open');
+}
+
+for(option of options) {
+    option.onclick = function() {
+        searchInput.placeholder = `Rechercher dans ${this.innerText.toLowerCase()}...`;
+    }
+}
---
+++ scriptLogin.js
@@ -0,0 +1,27 @@
+function showSignup(){
+    document.getElementById('loginForm').classList.remove('active');
+    document.getElementById('signup Form').classList.add('active');
+}
+
+document.getElementById('loginFormSubmit').addEventListener('submit', function(event){
+    event.preventDefault();
+});
+
+document.getElementById('signupFormSubmit').addEventListener('submit', function(event){
+    event.preventDefault();
+});
+
+function ValidateSignupForm(){
+    const password = document.querySelector ('#signupForm input[type="password"]').value;
+    const confirmPassword = document.querySelector('#signupForm input[type="password"]:last-of-type').value;
+    if(password !== confirmPassword){
+        alert('Les mots de passe ne correspondent pas. Veuillez réessayer.');
+        return false;
+    }
+
+    if(password.length < 8){
+        alert('Le mot de passe doit contenir au moins 8 caractères. Veuillez réessayer.');
+        return false;
+    }
+    return true;
+}
