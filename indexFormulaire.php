2ab360364745ec3fcc29cf6ce89e179de164aac1
Mary <marionbochud@gmail.com>
2026-06-09 08:41:26 +0200

    files created and del

--- indexFormulaire.php
+++ indexFormulaire.php
@@ -3,10 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Connection</title>
+    <title>Formulaire de contact</title>
 </head>
 <body>
-
     
 </body>
 </html>
